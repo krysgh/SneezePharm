361ef4a391ebedba6e919d7dab38ddf8268803b9
NataliaMZ-IT <zamperlinin@gmail.com>
2025-10-23 19:53:05 -0300

    Add classe e métodos do fornecedor

---
+++ SneezePharm/Fornecedor.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SneezePharm
+{
+    public class Fornecedor
+    {
+        public string Cnpj { get; private set; }
+        public string RazaoSocial { get; private set; }
+        public string Pais { get; private set; }
+        public DateOnly DataAbertura { get; private set; }
+        public DateOnly UltimoFornecimento { get; private set; }
+        public DateOnly DataCadastro { get; private set; } = DateOnly.FromDateTime(DateTime.Now);
+        public char Situacao { get; private set; } = 'A';
+
+        public Fornecedor(string cnpj,
+            string razaoSocial,
+            string pais,
+            DateOnly dataAbertura,
+            DateOnly ultimoFornecimento
+            )
+        {
+            this.Cnpj = cnpj;
+            this.RazaoSocial = razaoSocial;
+            this.Pais = pais;
+            this.DataAbertura = dataAbertura;
+            this.UltimoFornecimento = ultimoFornecimento;
+        }
+
+        public Fornecedor(string cnpj,
+            string razaoSocial,
+            string pais,
+            string dataAbertura,
+            string ultimoFornecimento,
+            string dataCadastro,
+            string situacao
+            )
+        {
+            this.Cnpj = cnpj;
+            this.RazaoSocial = razaoSocial.Trim();
+            this.Pais = pais.Trim();
+            this.DataAbertura = DateOnly.ParseExact(dataAbertura, "ddMMyyyy");
+            this.UltimoFornecimento = DateOnly.ParseExact(ultimoFornecimento, "ddMMyyyy"); ;
+            this.DataCadastro = DateOnly.ParseExact(dataCadastro, "ddMMyyyy"); ;
+            this.Situacao = char.Parse(situacao);
+        }
+
+        // Altera a situação do fornecedor de 'A' para 'I' e vice-versa
+        public void AlterarSituacao()
+        {
+            if (this.Situacao == 'A')
+            {
+                this.Situacao = 'I';
+            }
+            else
+            {
+                this.Situacao = 'A';
+            }
+        }
+
+        // Valida CNPJ por tamanho, por dígito verificador, e se tiver todos os números iguais
+        public static bool ValidarCNPJ(string cnpj)
+        {
+            int[] cnpjNumeros = cnpj.ToCharArray().Select(c => (int)char.GetNumericValue(c)).ToArray();
+            if (cnpjNumeros.Length != 14)
+            {
+                return false;
+            }
+            if (cnpjNumeros.Distinct().Count() == 1)
+            {
+                return false;
+            }
+
+
+            int calculo = (cnpjNumeros[0] * 6) + (cnpjNumeros[1] * 7) + (cnpjNumeros[2] * 8) + (cnpjNumeros[3] * 9) + (cnpjNumeros[4] * 2)
+                + (cnpjNumeros[5] * 3) + (cnpjNumeros[6] * 4) + (cnpjNumeros[7] * 5) + (cnpjNumeros[8] * 6) + (cnpjNumeros[9] * 7)
+                + (cnpjNumeros[10] * 8) + (cnpjNumeros[11] * 9);
+
+
+            calculo = calculo % 11;
+            if (calculo == 10)
+                calculo = 0;
+
+            if (calculo != cnpjNumeros[12])
+            {
+                return false;
+            }
+
+            calculo = (cnpjNumeros[0] * 5) + (cnpjNumeros[1] * 6) + (cnpjNumeros[2] * 7) + (cnpjNumeros[3] * 8) + (cnpjNumeros[4] * 9)
+                + (cnpjNumeros[5] * 2) + (cnpjNumeros[6] * 3) + (cnpjNumeros[7] * 4) + (cnpjNumeros[8] * 5) + (cnpjNumeros[9] * 6)
+                + (cnpjNumeros[10] * 7) + (cnpjNumeros[11] * 8) + (cnpjNumeros[12] * 9);
+
+            calculo = calculo % 11;
+            if (calculo == 10)
+                calculo = 0;
+
+            if (calculo != cnpjNumeros[13])
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        // Valida a data de abertura, retorna true se tiver pelo menos 2 anos; senão, retorna false
+        public static bool ValidarDataAbertura(DateOnly data)
+        {
+            data = data.AddYears(2);
+            if (data > DateOnly.FromDateTime(DateTime.Now))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Razão Social: {this.RazaoSocial}\n" +
+                $"CNPJ: {this.Cnpj}\n" +
+                $"País: {this.Pais}\n" +
+                $"Data de Abertura: {this.DataAbertura}\n" +
+                $"Data do Último Fornecimento: {this.UltimoFornecimento}\n" +
+                $"Data de Cadastro: {this.DataCadastro}\n" +
+                $"Situação: " + (this.Situacao == 'A' ? "Ativo" : "Inativo");
+        }
+
+        // Retorna todos os dados do fornecedor num string só para armazenar em arquivo
+        public string ToFile()
+        {
+            return $"{this.Cnpj}{this.RazaoSocial,-50}{this.Pais,-20}" +
+                this.DataAbertura.ToString().Replace("/", "") +
+                this.UltimoFornecimento.ToString().Replace("/", "") +
+                this.DataCadastro.ToString().Replace("/", "") +
+                this.Situacao;
+            ;
+        }
+    }
+}
---
+++ SneezePharm/Sistema.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SneezePharm
+{
+    public class Sistema
+    {
+        #region Listas de Dados
+        public List<Fornecedor> Fornecedores { get; set; } = [];
+        public List<string> FornecedoresBloqueados { get; set; } = [];
+        #endregion
+
+        #region Métodos Fornecedor
+        public void IncluirFornecedor()
+        {
+            Console.Write("Insere o cnpj da empresa fornecedor: ");
+            string cnpj = Console.ReadLine();
+            if (!Fornecedor.ValidarCNPJ(cnpj))
+            {
+                Console.WriteLine("CNPJ inválido! Retornando para menu...");
+                return;
+
+            }
+            if (LocalizarFornecedor(cnpj) is not null)
+            {
+                Console.WriteLine("CNPJ já cadastrada! Retornando para menu...");
+                return;
+            }
+
+            Console.Write("Insere a razão social da empresa: ");
+            string razaoSocial = Console.ReadLine();
+            if (razaoSocial.Length > 50)
+            {
+                razaoSocial = razaoSocial.Substring(0, 50);
+            }
+
+            Console.Write("Insere o país que a empresa está localizada: ");
+            string pais = Console.ReadLine();
+            if (pais.Length > 20)
+            {
+                pais = pais.Substring(0, 20);
+            }
+
+            Console.Write("Insere a data de abertura da empresa (formato ddMMyyyy): ");
+            if (!DateOnly.TryParseExact(Console.ReadLine(), "ddMMyyyy", out var dataAbertura))
+            {
+                Console.WriteLine("Data inválida! Cancelando operação...");
+                return;
+            }
+            if (!Fornecedor.ValidarDataAbertura(dataAbertura))
+            {
+                Console.WriteLine("A empresa precisa ter pelo menos 2 anos de abertura! Cancelando operação...");
+                return;
+            }
+
+            Console.Write("Insere a data do último fornecimento (formato ddMMyyyy): ");
+            if (!DateOnly.TryParseExact(Console.ReadLine(), "ddMMyyyy", out var dataFornecimento))
+            {
+                Console.WriteLine("Data inválida! Cancelando operação.");
+                return;
+            }
+            if (dataFornecimento > DateOnly.FromDateTime(DateTime.Now))
+            {
+                Console.WriteLine("A data do último fornecimento não pode ser no futuro. Cancelando operação...");
+                return;
+            }
+
+            Fornecedores.Add(new(cnpj, razaoSocial, pais, dataAbertura, dataFornecimento));
+        }
+
+        private Fornecedor? LocalizarFornecedor(string cnpj)
+        {
+            return Fornecedores.Find(f => f.Cnpj == cnpj);
+        }
+
+        public void ImprimirFornecedorLocalizado()
+        {
+            Console.WriteLine("Insere o CNPJ do fornecedor: ");
+            string cnpj = Console.ReadLine();
+            var fornecedor = LocalizarFornecedor(cnpj);
+            if (fornecedor is null)
+            {
+                Console.WriteLine("\nCNPJ não encontrado.");
+            }
+            else
+            {
+                Console.WriteLine("\n\tFornecedor\n" + fornecedor);
+            }
+        }
+
+        public void AlterarFornecedor() 
+        {
+            Console.WriteLine("Insere o CNPJ do fornecedor: ");
+            string cnpj = Console.ReadLine();
+            var fornecedor = LocalizarFornecedor(cnpj);
+            if (fornecedor is not null)
+            {
+                Console.WriteLine($"Deseja mesmo alterar a situação do fornecedor {fornecedor.RazaoSocial}? (0 - cancelar, 1 - confirmar)");
+                var confirma = Console.ReadLine() ?? "2";
+                if (confirma == "1")
+                {
+                    fornecedor.AlterarSituacao();
+                    Console.WriteLine("\nSituação do fornecedor alterado com sucesso!");
+                }
+                else 
+                {
+                    Console.WriteLine("\nOperação cancelada. Retornando para menu...");
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nCNPJ não encontrado.");
+            }
+        }
+
+        public void ImprimirFornecedores()
+        {
+            Console.WriteLine("-=-=- Lista de Fornecedores -=-=-");
+            if (Fornecedores.Count == 0)
+            {
+                Console.WriteLine("Nenhum fornecedor cadastrado.");
+            }
+            foreach (var fornecedor in Fornecedores)
+            {
+                Console.WriteLine(fornecedor + "\n");
+            }
+        }
+
+        public void BloquearFornecedor()
+        {
+            Console.WriteLine("Insere o CNPJ do fornecedor: ");
+            string cnpj = Console.ReadLine();
+            var fornecedor = LocalizarFornecedor(cnpj);
+            if (fornecedor is not null)
+            {
+                if (!FornecedoresBloqueados.Contains(cnpj))
+                {
+                    Console.WriteLine($"Deseja mesmo bloquear o fornecedor {fornecedor.RazaoSocial}? (0 - cancelar, 1 - confirmar)");
+                    var confirma = Console.ReadLine() ?? "";
+                    if (confirma == "1")
+                    {
+                        FornecedoresBloqueados.Add(fornecedor.Cnpj);
+                        Console.WriteLine("\nFornecedor bloqueado com sucesso!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Operação cancelada. Retornando para menu...");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\nEsse fornecedor já está bloqueado.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nCNPJ não encontrado.");
+            }
+            
+        }
+
+        public void DesbloquearFornecedor()
+        {
+            Console.WriteLine("Insere o CNPJ do fornecedor: ");
+            string cnpj = Console.ReadLine();
+            var fornecedor = LocalizarFornecedor(cnpj);
+            if (fornecedor is not null)
+            {
+                if (FornecedoresBloqueados.Contains(cnpj))
+                {
+                    Console.WriteLine($"Deseja mesmo desbloquear o fornecedor {fornecedor.RazaoSocial}? (0 - cancelar, 1 - confirmar)");
+                    var confirma = Console.ReadLine() ?? "";
+                    if (confirma == "1")
+                    {
+                        FornecedoresBloqueados.Remove(fornecedor.Cnpj);
+                        Console.WriteLine("\nFornecedor desbloqueado com sucesso!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nOperação cancelada. Retornando para menu...");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\nEsse fornecedor não está bloqueado.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nCNPJ não encontrado.");
+            }
+        }
+
+        public void ImprimirFornecedoresBloqueados()
+        {
+            Console.WriteLine("-=-=- Lista de Fornecedores  Bloqueados -=-=-");
+            if (FornecedoresBloqueados.Count == 0)
+            {
+                Console.WriteLine("Nenhum fornecedor bloqueado.");
+            }
+            foreach (var cnpj in FornecedoresBloqueados)
+            {
+                Console.WriteLine(LocalizarFornecedor(cnpj));
+            }
+        }
+        #endregion
+
+        #region Métodos Arquivos
+        private string CarregarFornecedor()
+        {
+            string diretorio = @"C:\SneezePharma\Files";
+            if (!Directory.Exists(diretorio))
+                Directory.CreateDirectory(diretorio);
+            string arquivoFornecedor = "Suppliers.data";
+            var diretorioFornecedor = Path.Combine(diretorio, arquivoFornecedor);
+            if (!File.Exists(diretorioFornecedor))
+            {
+                using (StreamWriter sw = File.CreateText(diretorioFornecedor))
+                {
+                    Console.WriteLine("Arquivo criado com sucesso");
+                    Console.ReadKey();
+                }
+            }
+            return diretorioFornecedor;
+        }
+
+        private string CarregarFornecedorBloqueado()
+        {
+            string diretorio = @"C:\SneezePharma\Files";
+            if (!Directory.Exists(diretorio))
+                Directory.CreateDirectory(diretorio);
+            string arquivoFornecedor = "RestrictedSuppliers.data";
+            var diretorioFornecedor = Path.Combine(diretorio, arquivoFornecedor);
+            if (!File.Exists(diretorioFornecedor))
+            {
+                using (StreamWriter sw = File.CreateText(diretorioFornecedor))
+                {
+                    Console.WriteLine("Arquivo criado com sucesso");
+                    Console.ReadKey();
+                }
+            }
+            return diretorioFornecedor;
+        }
+
+        public void LerArquivoFornecedor()
+        {
+            StreamReader reader = new(CarregarFornecedor());
+            using (reader)
+            {
+                List<Fornecedor> fornecedores = new();
+
+                while (reader.Peek() >= 0)
+                {
+                    var linha = reader.ReadLine();
+
+                    string cnpj = linha.Substring(0, 14);
+                    string nome = linha.Substring(14, 50);
+                    string pais = linha.Substring(64, 20);
+                    string dataAbertura = linha.Substring(84, 8);
+                    string dataFornecimento = linha.Substring(92, 8);
+                    string dataCadastro = linha.Substring(100, 8);
+                    string situacao = linha.Substring(108);
+
+
+                    Fornecedor fornecedor = new(
+                        cnpj,
+                        nome,
+                        pais,
+                        dataAbertura,
+                        dataFornecimento,
+                        dataCadastro,
+                        situacao
+                        );
+
+                    fornecedores.Add(fornecedor);
+                }
+                reader.Close();
+                this.Fornecedores = fornecedores;
+            }
+
+        }
+
+        public void LerArquivoFornecedorBloqueado()
+        {
+            StreamReader reader = new(CarregarFornecedorBloqueado());
+            using (reader)
+            {
+                List<string> bloqueados = new();
+
+                while (reader.Peek() >= 0)
+                {
+                    var linha = reader.ReadLine();
+
+                    bloqueados.Add(linha);
+                }
+                reader.Close();
+                this.FornecedoresBloqueados = bloqueados;
+            }
+
+        }
+
+        public void GravarArquivoFornecedor()
+        {
+            StreamWriter sw = new StreamWriter(CarregarFornecedor());
+
+            using (sw)
+            {
+                foreach (var fornecedor in this.Fornecedores)
+                {
+                    sw.WriteLine(fornecedor.ToFile());
+                }
+                sw.Close();
+            }
+        }
+
+        public void GravarArquivoFornecedorBloqueado()
+        {
+            StreamWriter sw = new StreamWriter(CarregarFornecedorBloqueado());
+
+            using (sw)
+            {
+                foreach (var bloqueado in this.FornecedoresBloqueados)
+                {
+                    sw.WriteLine(bloqueado);
+                }
+                sw.Close();
+            }
+        }
+        #endregion
+    }
+}
