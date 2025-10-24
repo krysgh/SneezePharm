6649d0b68988fd72141af17786ff318f1d804426
Bruno Nascimento <Bruno Nascimento@DESKTOP-3M9QPCB>
2025-10-24 11:52:18 -0300

    feat: add class `ItemProducao`

---
+++ SneezePharm/ItemProducao.cs
@@ -0,0 +1,43 @@
+﻿namespace SneezePharm;
+
+internal class ItemProducao
+{
+    public int IdProducao { get; private set; }
+    public string Principio { get; private set; }
+    public int QuantidadePrincipio { get; private set; }
+
+    public ItemProducao(int idProducao, string principio, int quantidadePrincipio)
+    {
+        IdProducao = idProducao;
+        Principio = principio;
+        QuantidadePrincipio = quantidadePrincipio;
+    }
+
+    public void SetPrincipio(string principio)
+    {
+        Principio = principio;
+    }
+
+    public void SetQuantidadePrincipio(int quantidadePrincipio)
+    {
+        QuantidadePrincipio = quantidadePrincipio;
+    }
+
+    public string ToFile()
+    {
+        string idProducao = IdProducao.ToString().PadLeft(5);
+        string idPrincipio = Principio.ToString().PadLeft(6);
+        string quantidadePrincipio = QuantidadePrincipio.ToString().PadLeft(4);
+
+        return $"{idProducao}{idPrincipio}{quantidadePrincipio}";
+    }
+
+    public override string ToString()
+    {
+        return $"\nId da Produção: {IdProducao}\n\r" +
+            $"Princípio: {Principio}\n\r" +
+            $"Quantidade de Princípio: {QuantidadePrincipio}\n\r";
+    }
+
+
+}
