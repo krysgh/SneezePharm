91c94b320200a0bf44c63fc8b1a53b237b138791
krysgh <krysthiang@gmail.com>
2025-10-24 14:49:47 -0300

    Classe VendaMedicamento criado

--- SneezePharm/Program.cs
+++ SneezePharm/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using SneezePharm;
+
 Console.WriteLine("Hello, World!");
+
+VendaMedicamento v = new VendaMedicamento("1234567890");
+
+Console.WriteLine(v.ToFile());
---
+++ SneezePharm/VendaMedicamento.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SneezePharm
+{
+    public class VendaMedicamento
+    {
+
+        #region Atributo
+        public int id = 1;
+        #endregion
+
+        #region Propriedades
+        public int Id { get; private set; }
+
+        public DateOnly DataVenda { get; private set; }
+
+        public string CPF { get; private set; }
+
+        public decimal ValorTotal { get; private set; }
+
+        #endregion
+
+        #region Construtores
+        //Construtor de leitura do aquivo para a lista
+        public VendaMedicamento(
+            string id,
+            string dataVenda,
+            string cpf,
+            string valorTotal
+            )
+        {
+            this.Id = Convert.ToInt32(id.Trim());
+            this.DataVenda = DateOnly.ParseExact(dataVenda,"ddMMyyyy");
+            this.CPF = cpf.Trim();
+            this.ValorTotal = Convert.ToDecimal(valorTotal);
+        }
+
+        //Construtor de captura (Incluir)
+        public VendaMedicamento(
+            string cpf
+            )
+        {
+            this.Id = id++;
+            this.CPF = cpf;
+            this.DataVenda = DateOnly.FromDateTime(DateTime.Now);
+            this.ValorTotal = 0;    
+        }
+        #endregion
+
+
+        
+
+        #region Alteracao
+        public void AlterarData(DateOnly dataVenda)
+        {
+            this.DataVenda = dataVenda;
+
+        }
+        #endregion
+
+        #region Exibicao
+
+        public override string ToString()
+        {
+            return $"ID da Venda: {this.Id}\n" +
+                $"Data da Venda: {this.DataVenda}\n" +
+                $"CPF do cliente: {this.CPF}\n" +
+                $"Valor Total? {this.ValorTotal}\n";
+        }
+
+        public string ToFile()
+        {
+            return this.Id.ToString().PadLeft(5) +
+                this.DataVenda.ToString().Replace("/", "") +
+                this.CPF +
+                this.ValorTotal.ToString("F2").Replace(',', '.').PadLeft(8);
+        }
+        #endregion
+    }
+}
